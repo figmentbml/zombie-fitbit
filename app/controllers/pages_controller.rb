52773c642ed0bb474fc825b7bf98b6241dad8712
figmentbml <beth.jaswa@gmail.com>
2015-01-28 21:01:18 -0700

    updated styling and added pics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
 
   def index
-    @user = User.find(1)
-    @user_zombie = User.find(2)
-    @user_data = Api.new(@user).user_info["user"]
-    @zombie_data = Api.new(@user_zombie).user_info["user"]
-    @api = Api.new(@user).activities_today
-    @api_zombie = Api.new(@user_zombie).activities_today
+    #@user = User.find(1)
+    #@user_zombie = User.find(2)
+    #@user_data = Api.new(@user).user_info["user"]
+    #@zombie_data = Api.new(@user_zombie).user_info["user"]
+    #api = Api.new(@user).activities_today
+    #@api_zombie = Api.new(@user_zombie).activities_today
   end
 
 end
